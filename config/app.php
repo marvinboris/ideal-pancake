859e6d545e483c05ff6688b50b1b714ec77f73e7
marvinboris <47755699+marvinboris@users.noreply.github.com>
2020-12-10 15:59:59 +0100

    Passport service provider

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          */
         Maatwebsite\Excel\ExcelServiceProvider::class,
         Barryvdh\Snappy\ServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
